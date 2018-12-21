402d3e9d9990e05565454e9da476a05c04776c30
loonytoon <itholmes@gmail.com>
2018-12-21 15:26:23 +0000

    Initial Class and Test setup.

---
+++ NU.StringCalculatorTest/StringCalculationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NU.StringCalculatorTest
+{
+    [TestClass]
+    public class StringCalculationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
