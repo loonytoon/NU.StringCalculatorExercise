3a31b025845533c87252a4b73bd40594f1fb570a
loonytoon <itholmes@gmail.com>
2019-01-07 09:30:34 +0000

    Methods and Tests for handling a string containing one integer

--- NU.StringCalculatorExercise/StringCalculator.cs
+++ NU.StringCalculatorExercise/StringCalculator.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NU.StringCalculatorExercise
 {
     public class StringCalculator
     {
+        private string _delimiter = ",";
+       
+
         public int Add(string numbers)
         {
-            return 0;
+            int total = 0;
+            try
+            {
+                if (numbers != string.Empty)
+                {
+                    string[] result = Regex.Split(numbers, _delimiter, RegexOptions.IgnoreCase);
+                    foreach (var value in result)
+                    {
+                        int number;
+                        bool success = int.TryParse(value, out number);
+                        if (success)
+                        {
+                            total = Add(total, number);
+                        }
+                        else
+                        {
+                            throw new System.ArgumentException("No numbers found in Parameter", "numbers");
+                        }
+                    }
+                    return total;
+                }
+                else
+                {
+                    return total;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
         }
     }
 }
--- NU.StringCalculatorTest/StringCalculationTests.UnitTests.cs
+++ NU.StringCalculatorTest/StringCalculationTests.UnitTests.cs
@@ -20,5 +20,22 @@ namespace NU.StringCalculatorTests
 
             Assert.AreEqual(_stringCalculator.Add(empty_string),0);
         }
+
+        [TestMethod]
+        public void When_A_String_Containing_A_Number_And_No_Delimiter_Is_Passed_Return_The_Number()
+        {
+            string no_delimiter = "1";
+
+            Assert.AreEqual(_stringCalculator.Add(no_delimiter), int.Parse(no_delimiter));
+        }
+
+        [TestMethod]
+        public void When_Two_Integers_Are_Passed_Their_Sum_Is_Return()
+        {
+            int number1 = 2;
+            int number2 = 3;
+            int total = 5;
+            Assert.AreEqual(_stringCalculator.Add(number1,number2), total);
+        }
     }
 }
